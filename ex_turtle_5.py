c4f304d3029a8258485d9fd9f87d02f98aa87561
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-06-25 21:48:33 +0530

    Create ex_turtle_5.py

---
+++ ex_turtle_5.py
@@ -0,0 +1,7 @@
+"""
+ Write a program to display the last digit of a number.
+(hint : any number % 10 will return the last digit) 
+"""
+num = int(input("Enter the number : "))
+last_digit = num%10
+print(last_digit)
